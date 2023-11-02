316021cb5a018aeb873970e829199de2a79e0300
Jagadeesh C <jagadeesh.cheluvaraj@gmail.com>
2023-11-02 21:59:55 +0530

    allure attachments

--- run_tests.bat
+++ run_tests.bat
@@ -1,3 +1,3 @@
 @echo off
-pipenv run pytest
+pipenv run pytest -v -m allure_options --clean-alluredir --disable-warnings
 pipenv run allure serve allure-results
--- tests/allure_test.py
+++ tests/allure_test.py
@@ -56,3 +56,18 @@ class TestAllureOptions():
     @allure.link("http://qameta.io", name="QAMETA", link_type="homepage")
     def test_custom_link(self):
         pass
+
+    def test_attach_body_with_default_kwargs(self):
+        allure.attach("Some content in plain text")
+
+    def test_attach_body(self):
+        xml_content = """<?xml version="1.0" encoding="UTF-8"?>
+        <tag>
+            <inside>...</inside>
+        </tag>
+        """
+        allure.attach(xml_content, name='some attachment name', attachment_type=allure.attachment_type.XML)
+        allure.attach("Some content in plain text")
+
+    def test_attach_file(self):
+        allure.attach.file(__file__)  # this file path
